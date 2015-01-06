4503908c91f8a9a21fb5b42c023cd1be06312805
Carlos Garnacho <carlosg@gnome.org>
2015-01-06 14:44:15 +0100

    gdkdevice: Add GdkDevice::tool-changed signal
    On the devices and backends that support it, this signal will be emitted
    on slave/floating devices whenever the tool they are interacting with
    changes. These notifications may also work as a sort of proximity events,
    as the tool will be unset when the pen moves too far.
    
    For backends, gdk_device_update_tool() has been included, all that should
    be done on their side is just calling this whenever any tool might have
    changed.

--- gdk/gdkdeviceprivate.h
+++ gdk/gdkdeviceprivate.h
@@ -71,6 +71,7 @@ struct _GdkDevice
 
   GdkSeat *seat;
   GPtrArray *tools;
+  GdkDeviceTool *last_tool;
 };
 
 struct _GdkDeviceClass
@@ -195,6 +196,8 @@ GdkDeviceTool *gdk_device_tool_new    (guint          serial);
 GdkDeviceTool *gdk_device_lookup_tool (GdkDevice     *device,
                                        guint          serial);
 void           gdk_device_add_tool    (GdkDevice     *device,
+                                       GdkDeviceTool *tool);
+void           gdk_device_update_tool (GdkDevice     *device,
                                        GdkDeviceTool *tool);
 
 GdkDeviceTool *gdk_device_tool_ref    (GdkDeviceTool *tool);
