391439192e6c4445f92f28d5ad00a5cca3d51e6e
William Hua <william.hua@canonical.com>
2015-10-05 23:48:18 -0400

    gdkwindow-win32: implement set_attach_params ()

--- gdk/win32/gdkwindow-win32.c
+++ gdk/win32/gdkwindow-win32.c
@@ -39,6 +39,7 @@
 #include "gdkvisualprivate.h"
 #include "gdkwin32window.h"
 #include "gdkglcontext-win32.h"
+#include "gdkattachparamsprivate.h"
 
 #include <cairo-win32.h>
 #include <dwmapi.h>
@@ -3468,6 +3469,7 @@ gdk_window_impl_win32_class_init (GdkWindowImplWin32Class *klass)
   impl_class->delete_property = _gdk_win32_window_delete_property;
   impl_class->create_gl_context = _gdk_win32_window_create_gl_context;
   impl_class->invalidate_for_new_frame = _gdk_win32_window_invalidate_for_new_frame;
+  impl_class->set_attach_params = gdk_window_move_using_attach_params;
 }
 
 HGDIOBJ
