97d2c47ecbee5109fa27e338ba2830982cc1d3e0
Carlos Soriano <csoriano@gnome.org>
2015-10-22 20:15:34 +0200

    Implement new GtkPathBar
    Following mockups and rewritting it completly to be able
    to be used by other projects like gnome-builder and dconf-editor
    and sanitize its code.
    This GtkPathBar will be only "visual", so no file management is done,
    and instead a subclass GtkFilesPathBar in a following patch will
    implement file management for the use case of GtkFileChooser and Nautilus.

--- gtk/gtk.h
+++ gtk/gtk.h
@@ -100,6 +100,7 @@
 #include <gtk/gtkfilechoosernative.h>
 #include <gtk/gtkfilechooserwidget.h>
 #include <gtk/gtkfilefilter.h>
+#include <gtk/gtkfilespathbar.h>
 #include <gtk/gtkflowbox.h>
 #include <gtk/gtkfontbutton.h>
 #include <gtk/gtkfontchooser.h>
@@ -152,6 +153,7 @@
 #include <gtk/gtkoverlay.h>
 #include <gtk/gtkpagesetup.h>
 #include <gtk/gtkpapersize.h>
+#include <gtk/gtkpathbar.h>
 #include <gtk/gtkpaned.h>
 #include <gtk/gtkplacessidebar.h>
 #include <gtk/gtkpopover.h>
